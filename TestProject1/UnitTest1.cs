85a5f19f4a809a24597bf166286a3bef5c179125
Georg Jung <gjung@gimas.net>
2025-02-03 19:51:19 +0100

    add test project

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var uut = new CoverletMissingAssembliesRepro.Test();
+            Assert.Equal("42", uut.GetTheAnswer());
+        }
+    }
+}
